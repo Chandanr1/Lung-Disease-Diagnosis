3d20013d4e15937fdc0f9d6a9ea87c4838861ed1
Chandan Kumar <89526399+Chandanr1@users.noreply.github.com>
2025-08-31 19:52:45 +0530

    Update artifacts_entity.py

--- xray/entity/artifacts_entity.py
+++ xray/entity/artifacts_entity.py
@@ -2,19 +2,19 @@ from dataclasses import dataclass
 from torch.utils.data.dataloader import DataLoader
 
 @dataclass
-class DataIngestionArtifacts:
+class DataIngestionArtifact:
     train_file_path: str
     test_file_path: str
 
 @dataclass
-class DataTransformationArtifacts:
+class DataTransformationArtifact:
     transformed_train_object: DataLoader
     transformed_test_object: DataLoader
     train_transform_file_path: str
     test_transform_file_path: str
 
 @dataclass
-class ModelTrainerArtifacts:
+class ModelTrainerArtifact:
    trained_model_path : str
 
 @dataclass
